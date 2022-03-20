fe120486d76b95a14ae6d002dca8eb2e8377eb62
Sebastian Mandal <sebastian.mandal@icloud.com>
2022-03-20 18:11:51 +0100

    Sample API version 1.0.0, still in the works
    Uploading to GitHub unfinished to have source control remotely

---
+++ src/1.0.0/api/Router.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express'
+import { Endpoint } from './types/Endpoint'
+
+const endpoints: Endpoint[] = [
+	{
+		method: 'get',
+		path: '/api/v1.0.0/test',
+		handler: (req: Request, res: Response) => {
+			res.send('API v1.0.0')
+		},
+	},
+]
+
+export default class Router {
+	private router: express.Router = express.Router()
+	private endpoints: Endpoint[]
+
+	constructor() {
+		this.endpoints = endpoints
+
+		this.endpoints.forEach((endpoint: Endpoint) => {
+			this.router[endpoint.method!](endpoint.path, endpoint.handler)
+		})
+	}
+
+	public get() {
+		return this.router
+	}
+}
---
+++ src/1.0.0/api/Server.ts
@@ -0,0 +1,17 @@
+import express, { Express } from 'express'
+import Router from './router'
+
+export default class Server {
+	public app: Express
+	private router: Router
+
+	constructor() {
+		this.app = express()
+		this.router = new Router()
+	}
+
+	public async start(port: number) {
+		this.app.use(this.router.get())
+		this.app.listen(port)
+	}
+}
